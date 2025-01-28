9d502eec9a6a3005be7ae7e9ff78c87dc56d7a2e
desobob11 <desmond.obrien@ucalgary.ca>
2025-01-28 11:23:12 -0700

    loading wheel

--- aeso-dash/src/Pages/SupplyDemand/SupplyDemand.js
+++ aeso-dash/src/Pages/SupplyDemand/SupplyDemand.js
@@ -15,7 +15,7 @@ function Ticker(props) {
       <div className="text-3xl text-center font-sans text-gray-900">
       {props.title}
       </div>
-      <div className={`text-3xl text-center  font-sans ${props.color}`}>
+      <div className={`flex justify-center text-3xl text-center  font-sans ${props.color}`}>
         {props.text}
       </div>
     </div>
@@ -34,19 +34,19 @@ function FuelTypeCard(props) {
         <div className="h-fit  text-xl">
         Max Capability:
         </div>
-        <div className="  h-fit text-xl text-blue-800">
+        <div className="  h-fit text-xl text-gray-900">
         {props.maxCap}MW
         </div>
         <div className="h-fit text-xl">
         Net Generation:
         </div>
-        <div className=" h-fit text-xl text-blue-800">
+        <div className=" h-fit text-xl text-gray-900">
         {props.netGen}MW
         </div>
         <div className="h-fit text-xl">
         Dispatched Contingency Reserve:
         </div>
-        <div className=" h-fit text-xl text-blue-800">
+        <div className=" h-fit text-xl text-gray-900">
         {props.maxCap}MW
         </div>
 
@@ -56,17 +56,37 @@ function FuelTypeCard(props) {
   );
 }
 
+function LoadingWheel(props) {
+  return (
+
+
+<div role="status">
+    <svg aria-hidden="true" className="r w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+    <span class="sr-only">Loading...</span>
+</div>
+
+
+  );
+}
+
 export default function SupplyDemand() {
 
     var supplyDemandData = {};
 
-    const [tickerList, setTickerList] = useState([]);
 
-    const tickers = {"total_net_generation": "Net Generation",
-                    "net_to_grid_generation": "Net-to-Grid",
+
+    const tickers = {"alberta_internal_load": "Alberta Internal Load",
                     "net_actual_interchange": "Net Interchange",
-                    "alberta_internal_load": "Alberta Internal Load"
+                    "net_to_grid_generation": "Net-to-Grid",
+                    "total_net_generation": "Net Generation"
     };
+
+    const [tickerList, setTickerList] = useState(
+      Object.values(tickers).map(x => <Ticker title={x} text={<LoadingWheel/>}/>)
+    );
 
     useEffect(() => {
       if (Object.keys(supplyDemandData).length == 0) {
@@ -117,8 +137,8 @@ export default function SupplyDemand() {
                  <div className="font-sans min-h-full">
      
      
-                     <header className="mt-16 w-full fixed bg-white shadow">
-                         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                     <header className=" w-full bg-white shadow">
+                         <div className=" h-32 w-full px-4 py-6 sm:px-6 lg:px-8">
                              <h1 className="grid grid-cols-4 gap-4 text-4xl font-bold tracking-tight text-gray-900">
                               {tickerList}
                              </h1>
